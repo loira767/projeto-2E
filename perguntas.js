2ca86985e5b44c17d1daf67dc5e4ad826cf346a1
Adni Segantin <142013506+loira767@users.noreply.github.com>
2024-10-18 14:28:00 -0300

    n

---
+++ perguntas.js
@@ -0,0 +1,19 @@
+criaCartao(
+    'Matemática',
+    'Qual a raiz quadrada de 144?',
+'A rais quadrada de 144 é 12'
+)
+criaCartao(
+    'Matemática',
+    'Quanto é 457+142?',
+    'O resultado da conta é 599'
+)
+criaCartao(
+    'Matematica',
+    'quanto vale pi?',
+    'pi vale 3,14'
+)
+criaCartao(
+    'quanto é 99-86?',
+    'O resultado da conta é 13'
+)
